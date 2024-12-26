4321011b62a7377c24cbd24f5595a168e898ff71
Takeru Seki <0084takeru@gmail.com>
2024-12-26 17:08:39 +0900

    Play Button, Video Pllayer, Single Movie Endpoint

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BsFillPlayFill } from "react-icons/bs";
 import FavoriteButton from "./FavoriteButton";
+import { useRouter } from "next/navigation";
 
 interface MovieCardProps {
   data: Record<string, any>;
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
+  const router = useRouter();
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
       <img
@@ -88,7 +90,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
                 transition
                 hover:bg-neutral-300
               "
-              onClick={() => {}}
+              onClick={() => router.push(`/watch/${data?.id}`)}
             >
               <BsFillPlayFill size={30} />
             </div>
